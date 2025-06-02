59e2e885c83573d87e7a180c8edaae83f83c8660
Angel Adeoye <angeladeoye@gmail.com>
2025-06-02 18:43:36 +0200

    Updates

--- compass_pipeline.py
+++ compass_pipeline.py
@@ -203,6 +203,7 @@ class CompASSPipeline(StableDiffusionPipeline):
                         # replaece with actuall loss function
                         # print(f"Number of centroids at timestep {i}: {len(self.attn_store.centroids)} with shape: {self.attn_store.centroids[0].shape}")
                         centroids = self.attn_store.get_eot_centroids(eot_tensor)
+                        print(centroids)
                         grid = self.attn_store.grid_cache[16 * latent_width / 4]
                         saliency_pred = centroids_to_kde(centroids, grid, sigma=0.01)
                         loss = divergence_loss(saliency_pred, target_map)
