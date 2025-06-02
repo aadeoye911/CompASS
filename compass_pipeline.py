1c906755cd733bdeacbac5e4f0336fb919cecd8d
Angel Adeoye <angeladeoye@gmail.com>
2025-06-02 18:10:43 +0200

    Updates

--- compass_pipeline.py
+++ compass_pipeline.py
@@ -209,7 +209,7 @@ class CompASSPipeline(StableDiffusionPipeline):
                         grid = self.attn_store.grid_cache[16 * latent_width / 4]
                         saliency_pred = centroids_to_kde(centroids, grid, sigma=0.03)
                         loss = divergence_loss(saliency_pred, target_map)
-                        grad_cond = torch.autograd.grad(loss, [latents], retain_graph=False)[0]
+                        grad_cond = torch.autograd.grad(loss, [latents], retain_graph=True)[0]
                         # loss.backward()
                 
                     ####################################################
